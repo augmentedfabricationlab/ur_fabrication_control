a34938f9769f712fdce1a8006a094857f5b8717b
begums <begums@users.noreply.github.com>
2021-10-07 10:49:40 +0200

    End effectors update (double check)

--- src/ur_fabrication_control/direct_control/urscript.py
+++ src/ur_fabrication_control/direct_control/urscript.py
@@ -532,19 +532,50 @@ class URScript(object):
         line = "\tmovec(p{}, p{}, v={}, r={})"
         self.add_line(line.format(pose_via, pose_to, velocity, radius))
 
-    def digital_out(self, number, value):
-        """
+    def add_digital_out(self, number, value):
+        """Assign a boolean value to a digital output.
+
+        Parameters
+        ----------
+        number : integer
+            Digital output number.
+
+        value : boolean
+
         """
         self.add_line("\tset_digital_out({}, {})".format(number, value))
 
     def areagrip_on(self, sleep=1.0):
+        """Turn the area gripper on.
         """
+        self.add_digital_out(6, True) # Digital output 6 is connected to Valve output 4, connected to the supply on the gripper.
+        self.add_digital_out(7, False) # Digital output 7 is connected to Valve output 2, connected to the blow-off on the gripper. (next to silencer)
+        self.add_line("\tsleep({})".format(sleep))
+
+    def areagrip_blowoff(self, sleep=1.0):
+        """Let the area gripper blow off.
         """
-        self.add_digital_out(7, True)
+        self.add_digital_out(6, True) # Digital output 6 is connected to Valve output 4, connected to the supply on the gripper.
+        self.add_digital_out(7, True) # Digital output 7 is connected to Valve output 2, connected to the blow-off on the gripper. (next to silencer)
         self.add_line("\tsleep({})".format(sleep))
 
     def areagrip_off(self, sleep=0.1):
+        """Turn the area gripper off.
         """
+        self.add_digital_out(6, False) # Digital output 6 is connected to Valve output 4, connected to the supply on the gripper.
+        self.add_digital_out(7, False) # Digital output 7 is connected to Valve output 2, connected to the blow-off on the gripper. (next to silencer)
+        self.add_line("\tsleep({})".format(sleep))
+
+    def parallelgrip_open(self, sleep=1.0):
+        """Open the parallel gripper.
         """
-        self.add_digital_out(7, False)
+        self.add_digital_out(6, True) # Digital output 6 is connected to Valve output 4, connected to the right side on the gripper.
+        self.add_digital_out(7, False) # Digital output 7 is connected to Valve output 2, connected to the left side on the gripper.
+        self.add_line("\tsleep({})".format(sleep))
+
+    def parallelgrip_close(self, sleep=1.0):
+        """Close the parallel gripper.
+        """
+        self.add_digital_out(6, False) # Digital output 6 is connected to Valve output 4, connected to the right side on the gripper.
+        self.add_digital_out(7, True) # Digital output 7 is connected to Valve output 2, connected to the left side on the gripper.
         self.add_line("\tsleep({})".format(sleep))
