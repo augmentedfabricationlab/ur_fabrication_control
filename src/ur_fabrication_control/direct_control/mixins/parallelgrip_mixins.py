a2459e714a4fe370bd6e60c5ff4b9a08f293fa8e
lidiatanasova <39670477+lidiatanasova@users.noreply.github.com>
2021-10-15 16:09:58 +0200

    update mixins
    area_gripp_mixins and parallelgrip_mixins

--- src/ur_fabrication_control/direct_control/mixins/parallelgrip_mixins.py
+++ src/ur_fabrication_control/direct_control/mixins/parallelgrip_mixins.py
@@ -4,14 +4,14 @@ import os
 
 
 class ParallelGripMixins:
-    def grip_open(self, sleep=1.0):
+    def parallelgrip_open(self, sleep=1.0):
         """Open the parallel gripper.
         """
         self.add_digital_out(6, True) # Digital output 6 is connected to Valve output 4, connected to the right side on the gripper.
         self.add_digital_out(7, False) # Digital output 7 is connected to Valve output 2, connected to the left side on the gripper.
         self.add_line("\tsleep({})".format(sleep))
 
-    def grip_close(self, sleep=1.0):
+    def parallelgrip_close(self, sleep=1.0):
         """Close the parallel gripper.
         """
         self.add_digital_out(6, False) # Digital output 6 is connected to Valve output 4, connected to the right side on the gripper.
