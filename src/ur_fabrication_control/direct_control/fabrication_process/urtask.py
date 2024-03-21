f65dd8bda983cb975552845eeb1c26a33dc7f9e1
Gido Dielemans <gido@dlmns.nl>
2024-03-21 16:35:03 +0100

    Update urtask.py

--- src/ur_fabrication_control/direct_control/fabrication_process/urtask.py
+++ src/ur_fabrication_control/direct_control/fabrication_process/urtask.py
@@ -1,6 +1,7 @@
 from fabrication_manager.task import Task
 from ur_fabrication_control.direct_control import URScript
 from ur_fabrication_control.direct_control.common import send_stop
+import time
 
 __all__ = [
     "URTask"
@@ -11,9 +12,12 @@ class URTask(Task):
         super(URTask, self).__init__(key)
         self.robot = robot
         self.robot_address = robot_address
+        self.rec_msg = "Task_{}_received".format(key)
         self.req_msg = "Task_{}_complete".format(key)
         self.sent = False
+        self.received = False
         self.server = None
+        self.nodes = []
 
     @classmethod
     def from_urscript(cls, robot, robot_address, urscript, key=None):
@@ -27,17 +31,32 @@ class URTask(Task):
         urtask.create_urscript_from_nodes(nodes)
         return urtask
 
-    def create_urscript_from_nodes(self, nodes):
+    def urscript_fabrication_header(self):
+        ## Initialize instance
         self.urscript = URScript(*self.robot_address)
         self.urscript.start()
+        
+        ## Set tool
         tool = self.robot.attached_tool
         self.urscript.set_tcp(list(tool.frame.point)+list(tool.frame.axis_angle_vector))
         self.urscript.add_line("textmsg('>> TASK{}.')".format(self.key))
         
-        # Find a way to replace the socket open command with recent server ip an port
+        ## Establish communication
         self.urscript.set_socket(self.server.ip, self.server.port, self.server.name)
         self.urscript.socket_open(self.server.name)
+        ## Send script received msg
+        self.urscript.socket_send_line_string(self.rec_msg, self.server.name)
 
+    def urscript_fabrication_footer(self):
+        ## Send script finished msg
+        self.urscript.socket_send_line_string(self.req_msg, self.server.name)
+        self.urscript.socket_close(self.server.name)
+
+        ## Add footer and generate script
+        self.urscript.end()
+        self.urscript.generate()   
+
+    def create_urscript_from_nodes(self, nodes):
         # currently assuming frames are in RCS
         for i, node in enumerate(nodes):
             if node.type == "linear":
@@ -48,22 +67,52 @@ class URTask(Task):
                 self.urscript.move_joint(node.joint_configuration, node.robot_vel)
             node_msg = {"TASK":self.key, "NODE":i}
             self.urscript.socket_send_line_string(str(node_msg), self.server.name)
+
+    def create_urscript(self):
+        self.create_urscript_from_nodes(self.nodes)
+
+    def _create_urscript(self):
+        self.urscript_fabrication_header()
+        self.create_urscript()
+        self.urscript_fabrication_footer()
+
+    def check_msg(self, msg):
+        return msg in self.server.msgs.values()
+
+    def run(self, stop_thread, attempts=2):
+        ## Create the urscript
+        self._create_urscript()
+
+        ## Send script with timeout and attempts clauses
+        timeout = time.time() + 10
+        while not self.received:
+            if not self.sent:
+                attempts -= 1
+                self.urscript.send_script()
+                self.log("URScript sent... attempts left {}".format(attempts))
+                self.sent = True
+                self.is_running = True
+            if self.check_msg(self.rec_msg) and not self.received:
+                self.received = True
+            if not self.received and time.time() > timeout:
+                if attempts > 0:
+                    self.sent = False
+                    timeout = time.time() + 10
+                else:
+                    self.log("FAULT... No attempts left... UR could not be reached")
+                    break
+            if stop_thread():
+                self.log("Forced to stop...")
+                self.sent = False
+                self.is_running = False
+                self.is_completed = False
+                break
         
-        self.urscript.socket_send_line_string(self.req_msg, self.server.name)
-        self.urscript.socket_close(self.server.name)
-
-        self.urscript.end()
-        self.urscript.generate()
-
-    def check_req_msg(self):
-        return self.req_msg in self.server.msgs.values()
-
-    def run(self, stop_thread):
-        if not self.sent:
-            self.urscript.send_script()
-            self.log("URScript sent...")
-            self.sent = True
-        while not self.check_req_msg():
+        ## Check if the script is finished or aborted
+        while self.is_running and self.received:
+            if self.check_msg(self.req_msg):
+                self.log("Finished")
+                self.is_running = False
             if stop_thread():
                 self.log("Forced to stop...")
                 send_stop(self.urscript.ur_ip, self.urscript.ur_port)
@@ -71,8 +120,6 @@ class URTask(Task):
                 self.is_running = False
                 self.is_completed = False
                 break
-            else:
-                pass
         else:
             self.is_completed = True
             return True
